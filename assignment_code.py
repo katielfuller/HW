9bbdde8800f5268487ea14e015fa4c275eb35f8d
katielfuller <katielfuller01@gmail.com>
2021-04-12 17:20:21 -0500

    This is where we will be writing code

---
+++ assignment_code.py
@@ -0,0 +1,26 @@
+linearSearch( lst, key ):
+""" Search for key in unsorted list lst.  Note that
+    the index + 1 is also the number of probes. """
+for i in range( len(lst) ):
+    if key == lst[i]:
+        return i
+return -1
+
+def binarySearch( lst, key ):
+    """ Search for key in sorted list lst. Return
+        a pair containing the index (or -low - 1)
+        and a count of number of probes. """
+    count = 0
+    low = 0
+    high = len(lst) - 1
+    while (high &gt;= low):
+        count += 1
+        mid = (low + high) // 2
+        if key &lt; lst[mid]:
+            high = mid - 1
+        elif key == lst[mid]:
+            return (mid, count)
+        else:
+            low = mid + 1
+    # Search failed
+    return (-low - 1, count)
